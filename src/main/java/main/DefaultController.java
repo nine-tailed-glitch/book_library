d6eb726c726da407a7c5e3fc71580ee0c5ca3638
Glitch <ntg-mailbox@internet.ru>
2022-10-10 15:23:35 +0300

    Chapter 12.15

--- src/main/java/main/DefaultController.java
+++ src/main/java/main/DefaultController.java
@@ -1,16 +1,34 @@
 package main;
 
 
+import main.model.Book;
+import main.model.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.ArrayList;
 
-@RestController
+
+//@RestController
+@Controller
 public class DefaultController {
 
+    @Autowired
+    BookRepository bookRepository;
+
     @RequestMapping("/")
-    public String index() {
-        return (new Date()).toString();
+    public String index(Model model) {
+
+        Iterable<Book> bookIterable = bookRepository.findAll();
+        ArrayList<Book> books = new ArrayList<Book>();
+        for (Book book : bookIterable) {
+            books.add(book);
+        }
+        model.addAttribute("books", books);
+        model.addAttribute("booksCount", books.size());
+        return "index";
     }
 }
